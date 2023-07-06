a9a7cfd698ea114b148606d2db8a57d980862cdf
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-07-06 18:22:44 +0200

    Readd here Translation modification example

--- code/gameviewsomething.pas
+++ code/gameviewsomething.pas
@@ -13,16 +13,19 @@ type
     { Components designed using CGE editor.
       These fields will be automatically initialized at Start. }
     MichalisViewport: TCastleViewport;
+    LabelFps: TCastleLabel;
+    BoxGoingUp: TCastleBox;
     DesignMouseWithHat1: TCastleTransformDesign;
     DesignMouseWithHat2: TCastleTransformDesign;
     DesignMouseWithHat3: TCastleTransformDesign;
     SoundSourceManual: TCastleSoundSource;
     SoundAlien: TCastleSound;
-    ButtonSave, ButtonLoad: TCastleButton;
+    MyButton, ButtonSave, ButtonLoad: TCastleButton;
   private
     procedure CollisionEnter(const CollisionDetails: TPhysicsCollisionDetails);
     procedure SaveGame(Sender: TObject);
     procedure LoadGame(Sender: TObject);
+    procedure MyButtonClick(Sender: TObject);
   public
     constructor Create(AOwner: TComponent); override;
     procedure Start; override;
@@ -48,6 +51,9 @@ begin
   inherited;
   { Executed once when view starts. }
 
+  MyButton.Caption := 'Click me!';
+  MyButton.OnClick := {$ifdef FPC}@{$endif} MyButtonClick;
+
   ButtonSave.OnClick := @SaveGame;
   ButtonLoad.OnClick := @LoadGame;
 end;
@@ -55,7 +61,19 @@ end;
 procedure TViewSomething.Update(const SecondsPassed: Single; var HandleInput: boolean);
 begin
   inherited;
-  { Executed every frame. }
+  { This virtual method is executed every frame (many times per second). }
+  Assert(LabelFps <> nil, 'If you remove LabelFps from the design, remember to remove also the assignment "LabelFps.Caption := ..." from code');
+  LabelFps.Caption := 'FPS: ' + Container.Fps.ToString;
+end;
+
+procedure TViewSomething.MyButtonClick(Sender: TObject);
+begin
+  WritelnLog('Button clicked');
+
+  // translation is 3D vector
+  // TVector3
+
+  BoxGoingUp.Translation := BoxGoingUp.Translation + Vector3(0, 1 , 0);
 end;
 
 function TViewSomething.Press(const Event: TInputPressRelease): Boolean;
@@ -98,6 +116,8 @@ end;
 
 procedure TViewSomething.CollisionEnter(const CollisionDetails: TPhysicsCollisionDetails);
 begin
+  { TODO: put enemies in a list (e.g. TCastleTransformList)
+    to make this code less error-prone. }
   if CollisionDetails.OtherTransform = DesignMouseWithHat1 then
     DesignMouseWithHat1.Exists := false;
   if CollisionDetails.OtherTransform = DesignMouseWithHat2 then
@@ -115,6 +135,7 @@ begin
   UserConfig.SetVector3('player_position', Pos);
   UserConfig.SetVector3('player_direction', Dir);
   UserConfig.SetVector3('player_up', Up);
+  UserConfig.SetValue('player_saved', true);
 
   UserConfig.Save;
 end;
@@ -125,9 +146,12 @@ var
 begin
   UserConfig.Load;
 
-  Pos := UserConfig.GetVector3('player_position', Vector3(0, 0, 0));
-  Dir := UserConfig.GetVector3('player_direction', Vector3(0, 0, -1));
-  Up := UserConfig.GetVector3('player_up', Vector3(0, 1, 0));
+  if UserConfig.GetValue('player_saved', false) then
+  begin
+    Pos := UserConfig.GetVector3('player_position');
+    Dir := UserConfig.GetVector3('player_direction');
+    Up := UserConfig.GetVector3('player_up');
+  end;
 
   MichalisViewport.Camera.SetWorldView(Pos, Dir, Up);
 end;
